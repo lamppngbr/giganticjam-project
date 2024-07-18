e5c50d3f4f01407cb52fcf7b75350dc3143d1e6d
Lamp <99483057+lamppngbr@users.noreply.github.com>
2024-07-18 13:50:05 -0300

    Add files via upload

---
+++ GiganticJam Project/Scripts/PlayerScript/PlayerScript.gd
@@ -0,0 +1,12 @@
+extends KinematicBody
+
+var direction := Vector3()
+const WALK_SPEED := 8
+const RUN_SPEED : = WALK_SPEED * 2
+
+
+func _physics_process(delta):
+	pass
+
+func mov_player(delta) -> void:
+	pass
